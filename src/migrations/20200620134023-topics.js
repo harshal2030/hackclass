e44d21594c357ccc2163c0b847db98ad5f92480f
harshal <harshal2030@outlook.com>
2020-06-20 19:28:47 +0530

    added undo

--- src/migrations/20200620134023-topics.js
+++ src/migrations/20200620134023-topics.js
@@ -20,6 +20,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-   return;
+   return (queryInterface.renameColumn('topics', 'className', 'class'));
   }
 };
